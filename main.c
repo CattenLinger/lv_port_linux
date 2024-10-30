bf72b6c682ac0b288b115d39929d230e9eeb9c1c
CattenLinger <cattenlinger@outlook.com>
2024-10-30 23:01:28 +0800

    Add X11 codes and change demo to music.

--- main.c
+++ main.c
@@ -61,6 +61,23 @@ static void lv_linux_disp_init(void)
 
     lv_sdl_window_create(width, height);
 }
+#elif LV_USE_X11
+static void linux_x11_on_close_callback(lv_event_t * e)
+{
+    /* Do nothing here */
+}
+
+static void lv_linux_disp_init(void)
+{
+    const int width = atoi(getenv("LV_SDL_VIDEO_WIDTH") ? : "800");
+    const int height = atoi(getenv("LV_SDL_VIDEO_HEIGHT") ? : "480");
+
+    lv_display_t * disp = lv_x11_window_create("LVGL X11 Simulation", width, height);
+    lv_display_add_event_cb(disp, linux_x11_on_close_callback, LV_EVENT_DELETE, disp);
+
+    LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image source.*/
+    lv_x11_inputs_create(disp, &mouse_cursor_icon);
+}
 #else
 #error Unsupported configuration
 #endif
@@ -73,8 +90,26 @@ int main(void)
     lv_linux_disp_init();
 
     /*Create a Demo*/
-    lv_demo_widgets();
-    lv_demo_widgets_start_slideshow();
+    // lv_demo_widgets();
+    // lv_demo_widgets_start_slideshow();
+
+    lv_demo_music();
+
+    // lv_demo_keypad_encoder();
+
+    // lv_demo_benchmark();
+
+    // lv_demo_flex_layout();
+
+    // lv_demo_multilang();
+
+    // lv_demo_scroll();
+
+    // lv_demo_transform();
+
+    // lv_demo_stress();
+
+    // lv_demos_show_help();
 
     /*Handle LVGL tasks*/
     while(1) {
